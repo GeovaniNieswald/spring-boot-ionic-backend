b54ab499fc7dc0aa1afae75dc772529a88fdabdc
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2018-12-22 15:42:42 -0200

    Criando repository e service para Categoria

---
+++ src/main/java/com/cursomc/geovaninieswald/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.cursomc.geovaninieswald.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.geovaninieswald.domain.Categoria;
+import com.cursomc.geovaninieswald.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
