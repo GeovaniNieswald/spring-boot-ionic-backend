23996e823d1750f28a08bb00e735c2f129610f57
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2018-12-24 16:42:50 -0200

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/cursomc/geovaninieswald/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.cursomc.geovaninieswald.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursomc.geovaninieswald.domain.Pedido;
+import com.cursomc.geovaninieswald.repositories.PedidoRepository;
+import com.cursomc.geovaninieswald.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
