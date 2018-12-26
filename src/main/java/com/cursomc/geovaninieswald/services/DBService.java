f08f0621bfc27634a94f94c15c5c6d3f39b2948f
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2018-12-26 17:37:00 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/cursomc/geovaninieswald/services/DBService.java
+++ src/main/java/com/cursomc/geovaninieswald/services/DBService.java
@@ -109,7 +109,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
-		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
+		Cliente cli1 = new Cliente(null, "Geovani Alex Nieswald", "geovani_alex@hotmail.com", "36378912377", TipoCliente.PESSOA_FISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
 		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
