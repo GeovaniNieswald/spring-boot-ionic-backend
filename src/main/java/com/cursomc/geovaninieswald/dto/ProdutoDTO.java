5bc2ffc696f3b3e0982acb1012ccf01f96c366a4
Geovani Alex Nieswald <geovaninieswald@gmail.com>
2018-12-26 13:48:58 -0200

    Busca de produtos por nome e categorias

---
+++ src/main/java/com/cursomc/geovaninieswald/dto/ProdutoDTO.java
@@ -0,0 +1,47 @@
+package com.cursomc.geovaninieswald.dto;
+
+import java.io.Serializable;
+
+import com.cursomc.geovaninieswald.domain.Produto;
+
+public class ProdutoDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+	private double preco;
+
+	public ProdutoDTO() {
+	}
+
+	public ProdutoDTO(Produto obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.preco = obj.getPreco();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+}
--- src/main/java/com/cursomc/geovaninieswald/repositories/ProdutoRepository.java
+++ src/main/java/com/cursomc/geovaninieswald/repositories/ProdutoRepository.java
@@ -1,11 +1,22 @@
 package com.cursomc.geovaninieswald.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.cursomc.geovaninieswald.domain.Categoria;
 import com.cursomc.geovaninieswald.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias, Pageable pageRequest);
+	
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	//Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
