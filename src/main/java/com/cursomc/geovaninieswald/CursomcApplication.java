35b18932c832321be0cacd84b0199e5228b98fd3
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-04 17:23:29 -0200

    Salvando primeiro arquivo no S3

--- src/main/java/com/cursomc/geovaninieswald/CursomcApplication.java
+++ src/main/java/com/cursomc/geovaninieswald/CursomcApplication.java
@@ -1,11 +1,17 @@
 package com.cursomc.geovaninieswald;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cursomc.geovaninieswald.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -13,5 +19,6 @@ public class CursomcApplication implements CommandLineRunner {
 	
 	@Override
 	public void run(String... args) throws Exception {
+		s3Service.uploadFile("/home/geovani/Imagens/geovani.png");
 	}
 }
